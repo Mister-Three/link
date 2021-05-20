5a7abc919c2247639e25b72bb14347ff916ca1d1
Mister-Three <81454774+Mister-Three@users.noreply.github.com>
2021-05-20 10:56:07 -0400

    Update link.py
    added support for more web browsers

--- link.py
+++ link.py
@@ -6,5 +6,12 @@ while True:
     webbrowser.register('chrome',
     None,
     webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
-    webbrowser.get('chrome').open(url)
+    if webbrowser.get('chrome'):
+        open(url)
+    elif webbrowser.get("firefox"):
+        open(url)
+    elif webbrowser.get("bing"):
+        open(url)
+    elif webbrowser.get("brave"):
+        open(url)
     print("Enjoy " + URL)
